5c55ef174bdea4857f1ccbdeff691dd560ad98fd
brookezkeene <brooke.z.keene@gmail.com>
2018-06-19 10:20:40 -0400

    "Dashboard added"

--- TestShiny.R
+++ TestShiny.R
@@ -15,75 +15,109 @@ library(rsconnect)
 library(tidyverse)
 library(tm)
 
-ui <- fluidPage(
-  # App title ----
-  titlePanel("Content-Based Recommender"),
-
-  sidebarLayout(
-    sidebarPanel(
-      # Create a dropdown menu
-      selectInput("programs", label = h3("Co-Curricular Activities"), 
-                         choices = list("Bass Connections" = 1, 
-                                        "Blueprint" = 2, 
-                                        "BOW" = 3, 
-                                        "Catalyst" = 4, 
-                                        "Co-Lab Grants" = 5, 
-                                        "Co-Lab Roots" = 6,
-                                        "Data+" = 7,
-                                        "DataFest" = 8,
-                                        "Duke Engineers for International Development (DEID)" = 9,
-                                        "Design for America" = 10,
-                                        "Domath" = 11,
-                                        "Duke Conservation Tech" = 12,
-                                        "Duke Electric Vehicles" = 13,
-                                        "Duke Energy Club" = 14,
-                                        "Duke In Chicago" = 15,
-                                        "Duke In Silicon Valley" = 16, 
-                                        "Duke Mobile App Development" =17,
-                                        "Duke Motorsports" = 18,
-                                        "Duke Robotics" = 19,
-                                        "Duke Startup Challenge" = 20,
-                                        "Enable" = 21,
-                                        "Engineering World Health" = 22,
-                                        "FEMMES" = 23,
-                                        "HackDuke" = 24,
-                                        "Ideate" = 25,
-                                        "Institute of Electrical and Electronics Engineers (IEEE)" = 26, 
-                                        "Innovation Co-Lab Studio" = 27,
-                                        "MUSER" = 28,
-                                        "Project Edge" = 29,
-                                        "Project Search" = 30,
-                                        "Smart Home" = 31,
-                                        "SPIRE" = 32,
-                                        "The Cube" = 33,
-                                        "Wiring With Women" = 34,
-                                        "Women in Computing" = 35),
-                  multiple = TRUE),
-      
-      # Create a text input box for Programs
-      #textInput("programs", label = h3("Co-Curricular Programs"), 
-                #value = "Enter co-curricular programs..."),
-      # Include clarifying text ----
-      helpText("Note: From the drop-down menu, select all of the co-curricular 
-                programs you have participated in during your time at Duke.") 
+ui <- 
+  dashboardPage(
+    dashboardHeader(title = "Duke Co-Curriculars",
+                    dropdownMenu(type = "messages",
+                                 messageItem(
+                                   from = "Catalyst",
+                                   message = "We would love to have you. Please come to our interest meeting on the 1st!"
+                                 ),
+                                 messageItem(
+                                   from = "New User",
+                                   message = "How do I get started?",
+                                   icon = icon("question"),
+                                   time = "13:45"
+                                 )
+                    ),
+                    dropdownMenu(type = "notifications",
+                                 notificationItem(
+                                   text = "5 new organizations recommended today",
+                                   icon("users")
+                                 ),
+                                 notificationItem(
+                                   text = "Profile completion at 86%",
+                                   icon = icon("exclamation-triangle"),
+                                   status = "warning"
+                                 )
+                    ),
+                    dropdownMenu(type = "tasks", badgeStatus = "success",
+                                 taskItem(value = 90, color = "green",
+                                          "Past Activities"
+                                 ),
+                                 taskItem(value = 17, color = "aqua",
+                                          "Interests"
+                                 )
+                    )
     ),
-    mainPanel(
-      DT::dataTableOutput("table")
-    )
-  ),
-  
-  hr(),
-  fluidRow(
-    verbatimTextOutput('user_programs')
+    dashboardSidebar(
+      sidebarMenuOutput("menu"),
+      sidebarMenu(
+        menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+        menuItem("Widgets", tabName = "widgets", icon = icon("th"), 
+                 badgeLabel = "new", badgeColor = "green")
+      )
+    ),
+    dashboardBody(
+      tabItems(
+        tabItem(tabName = "dashboard",
+                h2("Dashboard")
+        ),
+        tabItem(tabName = "widgets",
+                h2("Widgets"),
+                # Create a dropdown menu
+                selectInput("programs", label = h3("Co-Curricular Activities"), 
+                            choices = list("Bass Connections" = 1, 
+                                           "Blueprint" = 2, 
+                                           "BOW" = 3, 
+                                           "Catalyst" = 4, 
+                                           "Co-Lab Grants" = 5, 
+                                           "Co-Lab Roots" = 6,
+                                           "Data+" = 7,
+                                           "DataFest" = 8,
+                                           "Duke Engineers for International Development (DEID)" = 9,
+                                           "Design for America" = 10,
+                                           "Domath" = 11,
+                                           "Duke Conservation Tech" = 12,
+                                           "Duke Electric Vehicles" = 13,
+                                           "Duke Energy Club" = 14,
+                                           "Duke In Chicago" = 15,
+                                           "Duke In Silicon Valley" = 16, 
+                                           "Duke Mobile App Development" =17,
+                                           "Duke Motorsports" = 18,
+                                           "Duke Robotics" = 19,
+                                           "Duke Startup Challenge" = 20,
+                                           "Enable" = 21,
+                                           "Engineering World Health" = 22,
+                                           "FEMMES" = 23,
+                                           "HackDuke" = 24,
+                                           "Ideate" = 25,
+                                           "Institute of Electrical and Electronics Engineers (IEEE)" = 26, 
+                                           "Innovation Co-Lab Studio" = 27,
+                                           "MUSER" = 28,
+                                           "Project Edge" = 29,
+                                           "Project Search" = 30,
+                                           "Smart Home" = 31,
+                                           "SPIRE" = 32,
+                                           "The Cube" = 33,
+                                           "Wiring With Women" = 34,
+                                           "Women in Computing" = 35),
+                            multiple = TRUE),
+                
+                # Include clarifying text ----
+                helpText("Note: Select all the co-curricular programs you have participated 
+                         at Duke from the drop-down menu.")
+                )
+        ),
+        DT::dataTableOutput("table")
+      )
   )
-)
 
 
 server <- function(input, output) {
   output$user_programs <- renderPrint({ input$programs })
-  # For text input --> output$user_programs <- renderPrint({strsplit(input$programs, ", ")})
+
   output$table <- DT::renderDataTable({
-    
     # Load Data from Excel (in csv format)
     programs_tags <- read_csv("/Users/brookekeene/Documents/Duke University/Data+/Project_21/Tag_Words.csv")
     
